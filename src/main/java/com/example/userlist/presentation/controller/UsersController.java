ca7d14cce7397afdbd5d8fad9992b942b48b513c
Anna Babicheva <anna.babicheva@nure.ua>
2022-01-14 17:51:21 +0200

    Add service layer

--- src/main/java/com/example/userlist/presentation/controller/UsersController.java
+++ src/main/java/com/example/userlist/presentation/controller/UsersController.java
@@ -1,30 +1,34 @@
 package com.example.userlist.presentation.controller;
 
-import com.example.userlist.data.dao.UserDao;
 import com.example.userlist.domain.entity.User;
+import com.example.userlist.domain.service.UsersService;
 import com.example.userlist.presentation.entity.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
-    private final UserDao userDao;
+    private final UsersService usersService;
 
     private static final int STATUS_CODE_SUCCESS = 200;
     private static final int STATUS_CODE_ERROR = 500;
 
     @Autowired
-    public UsersController(UserDao userDao) {
-        this.userDao = userDao;
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
     }
 
-    private Response<Void> handleRequest(User user, Consumer<User> userConsumer) {
+    @FunctionalInterface
+    public interface ResponseHandler {
+        void accept(User user) throws Exception;
+    }
+
+    private Response<Void> handleRequest(User user, ResponseHandler userResponse) {
         try {
-            userConsumer.accept(user);
+            userResponse.accept(user);
             return new Response(user, STATUS_CODE_SUCCESS, null);
         } catch (Exception e) {
             return new Response(user, STATUS_CODE_ERROR, e.getMessage());
@@ -33,28 +37,28 @@ public class UsersController {
 
     @GetMapping("/getAll")
     public Response<List<User>> getAllUsers() {
-        List<User> users = userDao.getAll();
+        List<User> users = usersService.getAllUsers();
         return new Response<>(users, STATUS_CODE_SUCCESS, null);
     }
 
     @GetMapping("/getById")
     public Response<User> getById(@RequestParam("userId") int userId) {
-        User user = userDao.getById(userId);
+        User user = usersService.getUserById(userId);
         return new Response<>(user, STATUS_CODE_SUCCESS, null);
     }
 
     @PostMapping("/create")
-    public Response<Void> createUser(@RequestBody User user) {
-        return handleRequest(user, userDao::create);
+    public Response<Void> createUsers(@RequestBody User user) {
+        return handleRequest(user, usersService::createUser);
     }
 
     @PutMapping("/update")
-    public Response<Void> updateUser(@RequestBody User user) {
-        return handleRequest(user, userDao::update);
+    public Response<Void> updateUsers(@RequestBody User user) {
+        return handleRequest(user, usersService::updateUser);
     }
 
     @DeleteMapping("/delete")
-    public Response<Void> deleteUser(@RequestBody User user) {
-        return handleRequest(user, userDao::delete);
+    public Response<Void> deleteUsers(@RequestBody User user) {
+        return handleRequest(user, usersService::deleteUser);
     }
 }
